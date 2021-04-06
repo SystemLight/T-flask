8197c5a7fafb776c2efbe69385bd952ee1db23a5
SystemLight <1466335092@qq.com>
2021-04-06 10:08:12 +0800

    [2021-04-06]-添加新的内置依赖项

--- setup.py
+++ setup.py
@@ -1,31 +1,35 @@
 from setuptools import setup
 
+
+def read_me(path):
+    with open(path, "r", encoding="utf-8") as fp:
+        return fp.read()
+
+
 setup(
-    name="flask-project",
-    python_requires=">=3.5",
+    name="T-flask",
+    python_requires=">=3.7",
     version="1.0.0",
     author="SystemLight",
     author_email="1466335092@qq.com",
     maintainer="SystemLight",
     maintainer_email="1466335092@qq.com",
-    url="",
+    url="https://github.com/SystemLight/T-flask",
     license="MIT",
-    description="",
-    long_description="",
+    description="Flask project development template ",
+    long_description=read_me("README.md"),
     long_description_content_type='text/markdown',
-    download_url="",
+    download_url="https://codeload.github.com/SystemLight/T-flask/zip/refs/heads/master",
     install_requires="",
     platforms=["Windows", "Linux"],
-    keywords=[],
+    keywords=["flask", "template"],
     classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
     ],
     py_modules=[],
     scripts=[],
