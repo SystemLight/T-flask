ed3dfe12f0273057e0762486dce46adf02d963e9
SystemLight <1466335092@qq.com>
2021-08-13 12:00:31 +0800

    feat: 优化分片上传处理类

--- wsgi_simple.py
+++ wsgi_simple.py
@@ -109,7 +109,7 @@ class UploadError(Exception):
 
 class ChunkOptions:
 
-    def __init__(self):
+    def __init__(self, _on_finished=None):
         self.total_chunks = None
         self.offset = None
 
@@ -120,59 +120,98 @@ class ChunkOptions:
         self.file_name = None
         self.file_size = None
 
-    def is_end_block(self):
-        return self.chunk_id + 1 == self.total_chunks
+        self._on_finished = _on_finished
+
+        self.url_prefix = "/static/"
 
     @staticmethod
-    def from_flask_request():
-        options = ChunkOptions()
+    def from_flask_request(_on_finished=None):
+        options = ChunkOptions(_on_finished)
 
         options.total_chunks = int(request.form["totalChunks"])  # 总计块数量
-        options.offset = int(request.form["offset"])
+        options.offset = int(request.form["offset"])  # 文件偏移位置
 
-        options.chunk_block = request.files["chunkBlock"]
+        options.chunk_block = request.files["chunkBlock"]  # 当前块内容
         options.chunk_id = int(request.form["chunkID"])  # 当前块编号
 
-        options.file_id = request.form["fileID"]
-        options.file_name = request.form["fileName"]
-        options.file_size = int(request.form["fileSize"])
+        options.file_id = request.form["fileID"]  # 块存储唯一ID
+        options.file_name = request.form["fileName"]  # 文件原始名称
+        options.file_size = int(request.form["fileSize"])  # 文件原始总大小
 
         return options
+
+    def is_end_block(self):
+        """
+
+        判定当前块是否为最终块
+
+        :return:
+
+        """
+        return self.chunk_id + 1 == self.total_chunks
+
+    def render_url(self):
+        return f"{self.url_prefix}{self.file_name}"
+
+    def on_process(self, result, save_path):
+        """
+
+        进程块保存完毕时回调处理
+
+        :param save_path:
+        :param result:
+        :return:
+
+        """
+        pass
+
+    def on_finished(self, result, save_path):
+        """
+
+        最终块保存完毕时回调处理
+
+        :param save_path:
+        :param result:
+        :return:
+
+        """
+        if callable(self._on_finished):
+            self._on_finished(self, result, save_path)
 
 
 class SliceSaveFile:
 
-    def __init__(self, save_folder: str, url_prefix: str, options: ChunkOptions):
+    def __init__(self, options: ChunkOptions):
         self.options = options
-
-        self.file_name, self.file_ext = os.path.splitext(options.file_name)
-        self.save_file_name = f"{options.file_id}{self.file_ext}"
-        self.save_path = safe_join(save_folder, self.save_file_name)
-        self.url_path = f"{url_prefix}{self.save_file_name}"
-
-    def save(self):
-        if self.is_exists():
-            raise ExistsError("文件已经存在")
-
-        with open(self.save_path, "ab") as f:
-            f.seek(self.options.offset)
-            f.write(self.options.chunk_block.stream.read())
-
-        if self.options.is_end_block():
-            if os.path.getsize(self.save_path) != self.options.file_size:
-                raise UploadError("上传失败")
-
-        return {
-            "name": self.file_name,
-            "url": self.url_path,
-            "uid": str(uuid.uuid1()),
-            "status": "success"
-        }
+        self.save_path = f"static/{options.file_name}"
 
     def is_exists(self):
         if os.path.exists(self.save_path) and self.options.chunk_id == 0:
             return True
         return False
+
+    def save(self):
+        if self.is_exists():
+            raise ExistsError("文件已经存在")
+        with open(self.save_path, "ab") as f:
+            f.seek(self.options.offset)
+            f.write(self.options.chunk_block.stream.read())
+
+        result = {
+            "name": self.options.file_name,
+            "url": self.options.render_url(),
+            "uid": self.options.file_id,
+            "status": "success"
+        }
+
+        if self.options.is_end_block():
+            if os.path.getsize(self.save_path) != self.options.file_size:
+                raise UploadError("上传失败")
+            self.options.on_finished(result, self.save_path)
+        else:
+            self.options.on_process(result, self.save_path)
+
+        return result
 
 
 def make_error(msg="error", code=400, data=None):
